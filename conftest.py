bb9fa27a01dfe78a22f2929bdcb95d591bb6d554
Vladislav Kasperovich <vladislav.kasperovich02@gmail.com>
2026-02-26 16:17:20 +0000

    fixture added for the ingredients_list

--- conftest.py
+++ conftest.py
@@ -12,17 +12,18 @@ def db_connection():
     conn.seed()
     return conn
 
-
-ingredients_list = [
-    Ingredient(1, 'Chicken Breast', 1),
-    Ingredient(2, 'Red Chili', 5),
-    Ingredient(3, 'Garlic', 2),
-    Ingredient(4, 'Coconut Milk', 1),
-    Ingredient(5, 'Pasta', 1),
-    Ingredient(6, 'Parmesan Cheese', 1),
-    Ingredient(7, 'Black Pepper', 2),
-    Ingredient(8, 'Cumin', 3)
-]
+@fixture
+def ingredients_list():
+    return [
+        Ingredient(1, 'Chicken Breast', 1),
+        Ingredient(2, 'Red Chili', 5),
+        Ingredient(3, 'Garlic', 2),
+        Ingredient(4, 'Coconut Milk', 1),
+        Ingredient(5, 'Pasta', 1),
+        Ingredient(6, 'Parmesan Cheese', 1),
+        Ingredient(7, 'Black Pepper', 2),
+        Ingredient(8, 'Cumin', 3)
+    ]
 
 @fixture
 def recipes_list():
